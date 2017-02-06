5a0f0a94889003028b4f172389baac6f60b39e04
benallalhabib <benallalhabib@10.34.25.107>
2017-02-06 16:06:45 +0100

    premier ajout projet

---
+++ src/main/java/hei/devweb/wejog/servlets/AbstractGenericServlet.java
@@ -0,0 +1,30 @@
+package hei.devweb.wejog.servlets;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+public abstract class AbstractGenericServlet extends HttpServlet {
+	
+/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+protected TemplateEngine createTemplateEngine (HttpServletRequest request){
+	ServletContextTemplateResolver templateResolver =  new ServletContextTemplateResolver(request.getServletContext());
+	templateResolver.setTemplateMode(TemplateMode.HTML);
+	templateResolver.setPrefix("/WEB-INF/templates/");
+	templateResolver.setSuffix(".html");
+	templateResolver.setCharacterEncoding("UTF-8");
+	
+	TemplateEngine templateEngine = new TemplateEngine();
+	templateEngine.setTemplateResolver(templateResolver);
+	templateEngine.addDialect(new Java8TimeDialect());
+	return templateEngine;
+}
+}
---
+++ src/main/java/hei/devweb/wejog/servlets/RecoverypassewordconfirmationServlet.java
@@ -0,0 +1,34 @@
+package hei.devweb.wejog.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+/**
+ * Servlet implementation class HomeServlet
+ */
+@WebServlet("/recoverypasswordconfirmation")
+public class RecoverypassewordconfirmationServlet extends AbstractGenericServlet{
+	private static final long serialVersionUID = 1L;
+      
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest req, HttpServletResponse resp)
+	 */
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		TemplateEngine templateEngine = this.createTemplateEngine(req);
+		WebContext context = new WebContext(req, resp, req.getServletContext());
+		
+		templateEngine.process("recoveryPasswordConfirmation", context, resp.getWriter());
+	}
+
+
+}
+
+
