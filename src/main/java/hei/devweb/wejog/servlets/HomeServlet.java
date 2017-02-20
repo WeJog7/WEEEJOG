323eb6c6484dcf3495891b9d13b3a67434216f9d
benallalhabib <benallalhabib@10.34.24.135>
2017-02-20 16:40:23 +0100

    connexion

--- src/main/java/hei/devweb/wejog/servlets/HomeServlet.java
+++ src/main/java/hei/devweb/wejog/servlets/HomeServlet.java
@@ -14,7 +14,7 @@ import org.thymeleaf.context.WebContext;
 /**
  * Servlet implementation class HomeServlet
  */
-@WebServlet("/user/home")
+@WebServlet("/home")
 public class HomeServlet extends AbstractGenericServlet{
 	private static final long serialVersionUID = 1L;
 
