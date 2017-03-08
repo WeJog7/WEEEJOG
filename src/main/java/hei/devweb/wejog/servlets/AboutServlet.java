4889d87b6dc35bc67b30a664e031e154cebda8e6
guillaume <Jaden@LAPTOP-V1ONV0DG.home>
2017-03-08 16:10:30 +0100

    Progression connexion

--- src/main/java/hei/devweb/wejog/servlets/AboutServlet.java
+++ src/main/java/hei/devweb/wejog/servlets/AboutServlet.java
@@ -13,7 +13,7 @@ import org.thymeleaf.context.WebContext;
 /**
  * Servlet implementation class AboutServlet
  */
-@WebServlet("/about")
+@WebServlet("/user/about")
 public class AboutServlet extends AbstractGenericServlet{
 	
 	private static final long serialVersionUID = 1L;
