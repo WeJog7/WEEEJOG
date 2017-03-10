209eeb88948513ba17fc0317504e721e4cf3f201
guillaume <Jaden@LAPTOP-V1ONV0DG>
2017-03-10 15:09:08 +0100

    correction erreur

--- src/main/java/hei/devweb/wejog/servlets/DeconnexionServlet.java
+++ src/main/java/hei/devweb/wejog/servlets/DeconnexionServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = {"/user/deconnexion", "/admin/servlet"})
+@WebServlet(urlPatterns = {"/user/deconnexion", "/admin/deconnexion"})
 public class DeconnexionServlet extends HttpServlet{
 
 	/**
