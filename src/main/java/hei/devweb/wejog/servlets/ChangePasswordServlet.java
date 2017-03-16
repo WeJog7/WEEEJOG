bc917c1af7b8ca3c9e5a111bd377fa9804e1e7cc
guillaume <Jaden@LAPTOP-V1ONV0DG>
2017-03-16 23:28:09 +0100

    Début de la mise en place du changement de mot de passe

--- src/main/java/hei/devweb/wejog/servlets/ChangePasswordServlet.java
+++ src/main/java/hei/devweb/wejog/servlets/ChangePasswordServlet.java
@@ -1,6 +1,10 @@
 package hei.devweb.wejog.servlets;
 
 import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.time.LocalDate;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -8,6 +12,10 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
+
+import hei.devweb.wejog.entities.User;
+import hei.devweb.wejog.entities.VerifyRecaptcha;
+import hei.devweb.wejog.managers.UserService;
 
 /**
  * Servlet implementation class ChangePasswordServlet
@@ -22,8 +30,15 @@ public class ChangePasswordServlet extends AbstractGenericServlet {
 		TemplateEngine templateEngine = this.createTemplateEngine(req);
 		WebContext context = new WebContext(req, resp, req.getServletContext());
 		templateEngine.process("changePassword", context, resp.getWriter());
-
 	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        
+		String email = request.getParameter("email");
+		String oldPassword = request.getParameter("oldPassword");
+		String newPassword = request.getParameter("newPassword");
+		String newPasswordConfirmation = request.getParameter("newPasswordConfirmation");
+    }
 
 
 }
