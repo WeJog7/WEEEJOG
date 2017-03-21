89e6fdfb580d257a4f1c7edce18826add4632555
guillaume <Jaden@LAPTOP-V1ONV0DG.home>
2017-03-22 00:32:34 +0100

    MAJ

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -3,20 +3,13 @@
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
 
-import hei.devweb.wejog.dao.Userdao;
-import hei.devweb.wejog.impl.UserDaoImpl;
 import hei.devweb.wejog.managers.UserService;
 
 public class test {
 
 	public static void main(String[] args) throws NoSuchAlgorithmException, InvalidKeySpecException{
-
-		/*Userdao UserDao = new UserDaoImpl();
-		System.out.println(UserDao.getmotdepasse("test@test.fr"));
-		System.out.println(UserDao.getUser("test@test.fr").getMotdepasse());
-		System.out.println(UserDao.getUser("test@test.fr").getPrenom());*/
 		
-		/*System.out.println(UserService.getInstance().genererMotDePasse("1111"));*/
+		System.out.println(UserService.getInstance().genererMotDePasse("1111"));
 		
 		System.out.println(generate(7));
 
