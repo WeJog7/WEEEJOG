a270335d7727ed32ce92f1a2288eda034585c739
guillaume <Jaden@LAPTOP-V1ONV0DG.home>
2017-03-23 17:39:26 +0100

    MAJ javascript + sécurisation captcha page création compte et génération d'un nouveau mot de passe

--- src/main/webapp/js/javascript.js
+++ src/main/webapp/js/javascript.js
@@ -9,11 +9,6 @@ function suppressionusers(id) {
 }
 
 
-function recaptchaCallback() {
-    $('#validation').removeAttr('disabled');
-};
-
-
 function validRecaptcha(){
 	
     var verification = grecaptcha.getResponse();
@@ -28,20 +23,6 @@ function validRecaptcha(){
         return true; 
     }
 }
-
-
-/*function validRecaptcha(){
-	var recaptcha = document.getElementById("recaptcha").value;
-	
-	if(recaptcha != null){
-		return true;
-	}
-	
-	else{
-		alert('The captcha must be answered.');
-		return false;
-	}
-}*/
 
 
 function isValidForm(){
@@ -73,10 +54,11 @@ function testerRadio(radio) {
 	var ConfirmMail = document.getElementById("ConfirmMail").value; 
 	var password = document.getElementById("password").value; 
 	var confirmPassword = document.getElementById("confirmPassword").value;
+	var verification = grecaptcha.getResponse();
 
 	for (var i=0; i<radio.length;i++) {
 		if (radio[i].checked) {
-			if(mail == ConfirmMail && password == confirmPassword){
+			if(mail == ConfirmMail && password == confirmPassword && verification.length != 0){
 				return true;
 			}
 			else{
@@ -88,6 +70,10 @@ function testerRadio(radio) {
 					alert('You have entered two different passwords');
 					//document.getElementById("passwordCheck").innerHTML = "You have entered two different passwords";
 				}
+				if(verification.length == 0)
+			    {
+			    	alert('The captcha must be done.');
+			    }
 				return false;
 			}
 		}
@@ -128,27 +114,3 @@ function testerRadio(radio) {
 		}
 	}
 };*/
-
-/*function verifForm(f)
-{
-	var nomOk = verifNom(f.name);
-	var prenomOk = veriflastName(f.lastName);
-	var birthOk = verifbirth(f.birth);
-	var mailOk = verifmail(f.mail);
-	var ConfirmMailOk = verifConfirmMail(f.ConfirmMail);
-	var passwordOk = verifpassword(f.password);
-	var confirmPasswordOk = verifconfirmPassword(f.confirmPassword);
-
-
-	if(nomOk && prenomOk &&  birthOk && mailOk && ConfirmMailOk && passwordOk && confirmPasswordOk)
-		return true;
-	else
-	{
-		alert("Veuillez remplir correctement tous les champs");
-		return false;
-	}
-}*/
-
-
-
-
