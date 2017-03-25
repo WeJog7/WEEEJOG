4a3cc9a5cf9e6183f437a4af96e68dcee010ef4a
guillaume <Jaden@LAPTOP-V1ONV0DG>
2017-03-25 18:36:27 +0100

    Information pour l'empêchement du changement de mot de passe

---
+++ src/main/java/hei/devweb/wejog/servlets/ChangePasswordErrorServlet.java
@@ -0,0 +1,26 @@
+package hei.devweb.wejog.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+/**
+ * Servlet implementation class ChangePasswordErrorServlet
+ */
+@WebServlet(urlPatterns = {"/user/changePasswordError", "/admin/changePasswordError"})
+public class ChangePasswordErrorServlet extends AbstractGenericServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		TemplateEngine templateEngine = this.createTemplateEngine(req);
+		WebContext context = new WebContext(req, resp, req.getServletContext());
+
+		templateEngine.process("changePasswordError", context, resp.getWriter());
+	}
+
+}
--- src/main/java/hei/devweb/wejog/servlets/ChangePasswordServlet.java
+++ src/main/java/hei/devweb/wejog/servlets/ChangePasswordServlet.java
@@ -23,37 +23,37 @@ import hei.devweb.wejog.managers.UserService;
 @WebServlet(urlPatterns = {"/user/changePassword", "/admin/changePassword"})
 public class ChangePasswordServlet extends AbstractGenericServlet {
 	private static final long serialVersionUID = 1L;
-       
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
+
 		TemplateEngine templateEngine = this.createTemplateEngine(req);
 		WebContext context = new WebContext(req, resp, req.getServletContext());
 		templateEngine.process("changePassword", context, resp.getWriter());
 	}
-	
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        
+
 		String email = request.getParameter("email");
 		String oldPassword = request.getParameter("oldPassword");
 		String newPassword = request.getParameter("newPassword");
 		String newPasswordConfirmation = request.getParameter("newPasswordConfirmation");
-		
+
 		HttpServletRequest httpRequest = (HttpServletRequest) request;
 		User member = (User) httpRequest.getSession().getAttribute("user");
-		
+
 		try {
 			if(email.equals(member.getMail()) && UserService.getInstance().validerMotDePasse(member.getMail(), oldPassword) 
 					&& newPassword.equals(newPasswordConfirmation)){
-				
+
 				String typeOfMail = "changePassword";
 				UserService.getInstance().updatePassword(member.getIdusers(), newPassword);
 				EnvoiMessage.main(member.getMail(), member.getPrenom(), member.getNom(), newPassword, typeOfMail);
 				response.sendRedirect("changePasswordConfirmation");
 			}
-			
+
 			else{
-				response.sendRedirect("changePassword");
+				response.sendRedirect("changePasswordError");
 			}
 		} catch (WejogSecuriteException e) {
 			// TODO Auto-generated catch block
@@ -65,7 +65,7 @@ public class ChangePasswordServlet extends AbstractGenericServlet {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-    }
+	}
 
 
 }
