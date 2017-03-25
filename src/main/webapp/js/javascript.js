bae8726676b769d1b5e752ec06ad60a468169aeb
guillaume <Jaden@LAPTOP-V1ONV0DG>
2017-03-25 01:52:01 +0100

    Sécurisation saisie date dans createAccount avec javascript

--- src/main/webapp/js/javascript.js
+++ src/main/webapp/js/javascript.js
@@ -26,15 +26,15 @@ function validContact(){
 		if(email.replace(/ /g,"")==""){
 			alert('Please enter your email.');
 		}
-		
+
 		if(firstName.replace(/ /g,"")==""){
 			alert('Please enter your first name.');
 		}
-		
+
 		if(lastName.replace(/ /g,"")==""){
 			alert('Please enter your last name.');
 		}
-		
+
 		if(message.replace(/ /g,"")==""){
 			alert('Please enter a message.');
 		}
@@ -81,6 +81,7 @@ function testerRadio(radio) {
 	var password = document.getElementById("password").value; 
 	var confirmPassword = document.getElementById("confirmPassword").value;
 	var verification = grecaptcha.getResponse();
+	var flag=0;
 
 	for (var i=0; i<radio.length;i++) {
 		if (radio[i].checked) {
@@ -91,35 +92,46 @@ function testerRadio(radio) {
 			else{
 				if(firstName.replace(/ /g,"")==""){
 					alert('Please enter your first name.');
+					flag=1;
 				}
-				
-				if(lastName.replace(/ /g,"")==""){
+
+				if(lastName.replace(/ /g,"")=="" && flag==0){
 					alert('Please enter your last name.');
+					flag=1;
 				}
-				
-				if(datepicker.replace(/ /g,"")==""){
+
+				if(datepicker.replace(/ /g,"")=="" && flag==0){
 					alert('Please enter a date.');
+					flag=1;
 				}
 				
-				if(mail==""){
+				if(flag==0){
+					flag = CheckDate(datepicker);
+				}
+
+				if(mail=="" && flag==0){
 					alert('Please enter an email.');
+					flag=1;
 				}
-				
-				if(password==""){
+
+				if(password=="" && flag==0){
 					alert('Please enter a password.');
+					flag=1;
 				}
-				
-				if(mail != ConfirmMail){
+
+				if(mail != ConfirmMail && flag==0){
 					alert('Mails adress are not the same');
 					//document.getElementById("mailCheck").innerHTML = "Mails adress are not the same";
+					flag=1;
 				}
-				if(password != confirmPassword){
+				if(password != confirmPassword && flag==0){
 					alert('You have entered two different passwords');
 					//document.getElementById("passwordCheck").innerHTML = "You have entered two different passwords";
+					flag=1;
 				}
-				if(verification.length == 0)
-				{
+				if(verification.length == 0 && flag==0){
 					alert('The captcha must be done.');
+					flag=1;
 				}
 				return false;
 			}
@@ -129,4 +141,38 @@ function testerRadio(radio) {
 			return false;
 		}
 	}
+};
+
+
+function CheckDate(datepicker) {
+	var dateNow = new Date();
+	var yearMin= dateNow.getFullYear()-100;
+	var yearMax= dateNow.getFullYear();
+	var separateur="-";
+	var a=(datepicker.substring(0,4));
+	var m=(datepicker.substring(5,7));
+	var j=(datepicker.substring(8));
+	var flag=0;
+
+	if((!isNaN(datepicker.substring(4,5))||!isNaN(datepicker.substring(7,8))&& flag==0)){
+		alert("Wrong format, please enter a correct date"); flag=1;
+	}
+	
+	if((datepicker.substring(4,5)!=separateur||datepicker.substring(7,8)!=separateur) && flag==0){
+		alert("Delimiters must be like "+separateur); flag=1;
+	}
+	
+	if((isNaN(a)||(a<yearMin)||(a>yearMax)) && flag==0){
+		alert("In the date, the year is not correct."); flag=1;
+	}
+
+	if((isNaN(m)||(m<1)||(m>12)) && flag==0){
+		alert("In the date, the month is not correct."); flag=1;
+	}
+
+	if((isNaN(j)||(j<1)||(j>31)) && flag==0){
+		alert("In the date, the day is not correct."); flag=1;
+	}
+
+	return flag;
 };
