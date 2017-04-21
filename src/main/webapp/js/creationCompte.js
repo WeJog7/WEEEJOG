df709227a2fe5411cdc9cab12e3577116a09ef34
guillaumeminaud <guillaume.minaud@hei.yncrea.fr>
2017-04-22 01:27:16 +0200

    CSS et javascript pour améliorer le design formulaire création compte

--- src/main/webapp/js/creationCompte.js
+++ src/main/webapp/js/creationCompte.js
@@ -1,10 +1,161 @@
+function validFirstName(){
+	var value = document.getElementById("name").value;
+	
+	if(value.replace(/ /g,"")!="")
+	{
+		document.getElementById("check_name1").style.display = "inline";
+		document.getElementById("check_name2").style.display = "none";
+		return true;
+	}
+	else
+	{
+		document.getElementById("check_name1").style.display = "none";
+		document.getElementById("check_name2").style.display = "inline";
+		return false;
+	}
+}
+
+
+function validLastName(){
+	var value = document.getElementById("lastName").value;
+	
+	if(value.replace(/ /g,"")!="")
+	{
+		document.getElementById("check_lastName1").style.display = "inline";
+		document.getElementById("check_lastName2").style.display = "none";
+		return true;
+	}
+	else
+	{
+		document.getElementById("check_lastName1").style.display = "none";
+		document.getElementById("check_lastName2").style.display = "inline";
+		return false;
+	}
+}
+
+
+function validDate(){
+	var value = document.getElementById("datepicker").value;
+	
+	if(value.replace(/ /g,"")!="")
+	{
+		document.getElementById("check_date1").style.display = "inline";
+		document.getElementById("check_date2").style.display = "none";
+		return true;
+	}
+	else
+	{
+		document.getElementById("check_date1").style.display = "none";
+		document.getElementById("check_date2").style.display = "inline";
+		return false;
+	}
+}
+
+
+function validEmail(){
+	var mail = document.getElementById("mail").value;
+	var confirmMail = document.getElementById("confirmMail").value;
+	
+	if(mail.replace(/ /g,"")!="")
+	{
+		document.getElementById("check_email1").style.display = "inline";
+		document.getElementById("check_email2").style.display = "none";
+		
+		if(mail==confirmMail){
+			document.getElementById("check_confirmMail1").style.display = "inline";
+			document.getElementById("check_confirmMail2").style.display = "none";
+			document.getElementById("errorConfirmMail").style.display = "none";
+		}
+		return true;
+	}
+	else
+	{
+		document.getElementById("check_email1").style.display = "none";
+		document.getElementById("check_email2").style.display = "inline";
+		return false;
+	}
+}
+
+
+function validConfirmEmail(){
+	var confirmMail = document.getElementById("confirmMail").value;
+	var mail = document.getElementById("mail").value;
+	
+	if(confirmMail.replace(/ /g,"")!="" && confirmMail==mail)
+	{
+		document.getElementById("check_confirmMail1").style.display = "inline";
+		document.getElementById("check_confirmMail2").style.display = "none";
+		document.getElementById("errorConfirmMail").style.display = "none";
+		return true;
+	}
+	else
+	{
+		document.getElementById("check_confirmMail1").style.display = "none";
+		document.getElementById("check_confirmMail2").style.display = "inline";
+		document.getElementById("errorConfirmMail").style.display = "inline";
+		return false;
+	}
+}
+
+
+function validPassword(){
+	var password = document.getElementById("password").value;
+	var confirmPassword = document.getElementById("confirmPassword").value;
+	
+	if(password.replace(/ /g,"")!="")
+	{
+		document.getElementById("check_password1").style.display = "inline";
+		document.getElementById("check_password2").style.display = "none";
+		
+		if(password==confirmPassword){
+			document.getElementById("check_confirmPassword1").style.display = "inline";
+			document.getElementById("check_confirmPassword2").style.display = "none";
+			document.getElementById("errorConfirmPassword").style.display = "none";
+		}
+		return true;
+	}
+	else
+	{
+		document.getElementById("check_password1").style.display = "none";
+		document.getElementById("check_password2").style.display = "inline";
+		return false;
+	}
+}
+
+
+function validConfirmPassword(){
+	var confirmPassword = document.getElementById("confirmPassword").value;
+	var password = document.getElementById("password").value;
+	
+	if(confirmPassword.replace(/ /g,"")!="" && confirmPassword==password)
+	{
+		document.getElementById("check_confirmPassword1").style.display = "inline";
+		document.getElementById("check_confirmPassword2").style.display = "none";
+		document.getElementById("errorConfirmPassword").style.display = "none";
+		return true;
+	}
+	else
+	{
+		document.getElementById("check_confirmPassword1").style.display = "none";
+		document.getElementById("check_confirmPassword2").style.display = "inline";
+		if(confirmPassword==password){
+			document.getElementById("cerrorConfirmPassword").style.display = "none";
+		}
+		else{
+			document.getElementById("errorConfirmPassword").style.display = "inline";
+		}
+		return false;
+	}
+}
+
+
 function testerRadio(radio) {
 
 	var firstName = document.getElementById("name").value;
 	var lastName = document.getElementById("lastName").value;
 	var datepicker = document.getElementById("datepicker").value;
 	var mail = document.getElementById("mail").value;
-	var ConfirmMail = document.getElementById("ConfirmMail").value; 
+	var confirmMail = document.getElementById("confirmMail").value; 
 	var password = document.getElementById("password").value; 
 	var confirmPassword = document.getElementById("confirmPassword").value;
 	var verification = grecaptcha.getResponse();
@@ -50,8 +201,8 @@ function testerRadio(radio) {
 			flag=1;
 		}
 
-		if(mail != ConfirmMail && flag==0){
-			alert('Mails adress are not the same');
+		if(mail != confirmMail && flag==0){
+			alert('Mails address are not the same.');
 			//document.getElementById("mailCheck").innerHTML = "Mails adress are not the same";
 			flag=1;
 		}
